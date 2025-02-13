3b163aa21a774872d6c819fab4c9b382f1d732ca
IgorNakajima <nakajimaigor@gmail.com>
2025-02-13 15:01:50 -0300

    atualizando tela de login

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './LoginPage.css'
 import Navbar from '../../components/Navbar/NavBar'
 import Footer from '../../components/Footer/Footer'
-export default function LoginPage() {
+
+function LoginPage() {
+    const [formData, setFormData] = useState({
+        nome: '',
+        idade: '',
+        email: '',
+        senha: '',
+        cpf: '',
+    });
     return (
         <>
             <Navbar />
-            <div className='container'>
-                <form>
-                    <h1>Cadastro</h1>
-                    <input placeholder="Nome" name='nome' type='text' />
-                    <input placeholder="Idade" name='idade' type='number' />
-                    <input placeholder="email" name='email' type='email' />
-                    <input placeholder="Senha" name='senha' type='password' />
-                    <input placeholder="Digite seu cpf" name='cpf' type='number' />
-                    <input placeholder="Endereço" name='endereço' type='text' />
-
-                    <form>
-                        <select name="plataforma">
-                            <option value="">Sexo</option>
-                            <option value="Masculino">Masculino</option>
-                            <option value="Feminino">Feminino</option>
-                            <option value="Outros">Prefiro não opinar</option>
-                        </select>
-                    </form>
-                    <button type='button' id='button-cadastrar'>Cadastrar</button>
-                </form>
+            <div className="mainlogin">
+                <div className="leftlogin">
+                </div>
+                <div className="right-login">
+                    <div className="card-login">
+                        <h1>Login</h1>
+                        <div className="textfield">
+                            <label for="usuario">Usuário</label>
+                            <input type="text" name="usuario" placeholder="Usuário" />
+                        </div>
+                        <div className="textfield">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" placeholder="Email" />
+                        </div>
+                        <div className="textfield">
+                            <label for="senha">Senha</label>
+                            <input type="text" name="senha" placeholder="Senha" />
+                        </div>
+                        <div className="textfield">
+                            <label for="cpf">Cpf</label>
+                            <input type="text" name="cpf" placeholder="Cpf" />
+                        </div>
+                        <button className="btn-login">Logar</button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
-    )
+    );
 }
+
+export default LoginPage;
