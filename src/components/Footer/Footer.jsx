1d53cc5fad18e0a26a55edc9b420963b441991e1
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2025-02-20 14:47:17 -0300

    refazedno footer e mexendo na navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { FaFacebook, FaInstagram, FaLinkedin, FaPaperPlane } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="footer-logo" style={{ marginLeft: '-30px' }}>
-          <img src="src/img/FooterLogo.png" alt="Logo do Hospital Saint-Michel" style={{ width: '15rem', marginRight: '5rem' }} />
+      <div className="container footer-container">
+        <div className="footer-logo">
+          <img src="src/img/FooterLogo.png" alt="Logo do Hospital Saint-Michel" style={{marginRight: '12rem'}}/>
         </div>
-        <br />
-        <div style={{ marginRight: '8rem' }}>
+
+        <div className="footer-section">
           <h3>Links Importantes</h3>
           <ul>
             <li><a href="/agendamentos">Agendamentos</a></li>
@@ -20,35 +20,26 @@ export default function Footer() {
           </ul>
         </div>
 
-        <div style={{ marginRight: '6rem' }}>
+        <div className="footer-section">
           <h3>Fale Conosco</h3>
           <p><strong>Telefone:</strong> (11) 6818-1255</p>
           <p><strong>Email:</strong> saintmichel@gmail.com</p>
           <p><strong>Endereço:</strong> Av. Marechal Tito, 3400</p>
           <p>Brasil</p>
         </div>
-
-        <div>
-          <h3>Informações</h3>
-          <div className="newsletter-box">
-            <input type="email" placeholder="Digite seu email aqui" />
-            <button><FaPaperPlane /></button>
-          </div>
-        </div>
       </div>
 
       <hr />
 
-      <div className="footer-bottom">
-        <p>© 2025 Direitos reservados Hospital Saint-Michel by PNTEC-LTD</p>
+      <div className="footer-bottom"  style={{marginLeft: '18rem'}}>
+        <p className="direitos">© 2025 Direitos reservados Hospital Saint-Michel by PNTEC-LTD</p>
         <div>
-          <span>Parcerias:</span> <strong>Libbs</strong>
+          <span>Parcerias:</span> <strong className="libbs-footer">Libbs</strong>
         </div>
         <div className="footer-social">
-          <a href="https://www.instagram.com/hospital.saintmichel/" target="_blank"><FaLinkedin /></a>
-          <a href="https://www.instagram.com/hospital.saintmichel/" target="_blank"><FaFacebook /></a>
+          <a href="https://www.linkedin.com/hospital.saintmichel/" target="_blank"><FaLinkedin /></a>
+          <a href="https://www.facebook.com/hospital.saintmichel/" target="_blank"><FaFacebook /></a>
           <a href="https://www.instagram.com/hospital.saintmichel/" target="_blank"><FaInstagram /></a>
-
         </div>
       </div>
     </footer>
