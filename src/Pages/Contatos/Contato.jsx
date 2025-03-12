4887dc053934af72c08b41814eeb6fd3d547d675
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2025-03-12 15:45:05 -0300

    Setando mapa

--- src/Pages/Contatos/Contato.jsx
+++ src/Pages/Contatos/Contato.jsx
@@ -16,20 +16,20 @@ const Contatos = () => {
     <>
       <Navbar />
       <img src="../src/img/Contatos.png" className="img-servicos" alt="Logo Servicos" />
-<br /><br />
-        <div className="map-container">
-          <iframe
-            title="Localização"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.0192186343984!2d-122.41941518468147!3d37.77492977975914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808cba3f0e2f%3A0x4c4e4c4c4c4c4c4c4c!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sbr!4v1615395398274!5m2!1sen!2sbr"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-          ></iframe>
-        </div>
+      <br /><br />
+      <div className="map-container">
+        <iframe
+          title="Localização"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.1234567890123!2d-46.4110!3d-23.5000!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0!2sAv.+Marechal+Tito,+3400!5e0!3m2!1sen!2sbr!4v1615395398274!5m2!1sen!2sbr"
+          width="100%"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+        ></iframe>
+      </div>
 
-<br />
+      <br />
       <div className="contato-container">
         <h2 className="titulo-contato">ENTRE EM CONTATO</h2>
         <h3 className="subtitulo-contato">Contato</h3>
