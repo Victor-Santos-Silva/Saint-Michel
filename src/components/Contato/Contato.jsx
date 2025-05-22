6efadf83dbae58b5eef4195493b9663253eff516
Victor dos Santos <victorsantos.goal@gmail.com>
2025-05-22 15:29:42 -0300

    atualizando as imagens

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -3,10 +3,10 @@ import "./Contato.css";
 import { useTheme } from '../../context/ThemeContext'; // Importe o hook do tema
 
 const ContactInfo = [
-  { title: "EMERGÊNCIA", info: "(11) 6918-1525", img: "../src/img/icons8-siren-96.png" },
-  { title: "LOCALIZAÇÃO", info: "Av. Marechal Tito, 3400", img: "../src/img/LocationIcon.png" },
-  { title: "E-MAIL", info: "saintmichiel@gmail.com", img: "../src/img/emailicon.png" },
-  { title: "HORÁRIO DE TRABALHO", info: "Dom-Dom 24 Horas", img: "../src/img/icons8-clock-50.png" }
+  { title: "EMERGÊNCIA", info: "(11) 6918-1525", img: "../img/icons8-siren-96.png" },
+  { title: "LOCALIZAÇÃO", info: "Av. Marechal Tito, 3400", img: "../img/LocationIcon.png" },
+  { title: "E-MAIL", info: "saintmichiel@gmail.com", img: "../img/emailicon.png" },
+  { title: "HORÁRIO DE TRABALHO", info: "Dom-Dom 24 Horas", img: "../img/icons8-clock-50.png" }
 ];
 
 export default function Contato() {
@@ -15,15 +15,15 @@ export default function Contato() {
   return (
     <>
       <div className={`contact-section ${darkMode ? 'dark' : ''}`}>
-        <p  className="titulo">ENTRE EM CONTATO</p>
+        <p className="titulo">ENTRE EM CONTATO</p>
         <h3 className="CONTATO">Contato</h3>
         <div className="contact-grid">
           {ContactInfo.map((item, index) => (
             <div key={index} className={`contact-card ${darkMode ? 'dark' : ''}`}>
               <div className="contact-img">
-                <img 
-                  src={item.img} 
-                  alt={item.title} 
+                <img
+                  src={item.img}
+                  alt={item.title}
                   className={darkMode ? 'dark-icon' : ''} // Classe para ícones
                 />
               </div>
