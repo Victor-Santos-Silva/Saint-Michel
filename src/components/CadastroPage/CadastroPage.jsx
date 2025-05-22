1748635238f7b1474ca2e792c4aa887068cf7478
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2025-05-22 15:55:00 -0300

    update

--- src/components/CadastroPage/CadastroPage.jsx
+++ src/components/CadastroPage/CadastroPage.jsx
@@ -342,7 +342,7 @@ function CadastroPage() {
                             />
                         </div>
                     </>
-                    
+                
                 );
             default:
                 return null;
